0a2567472da0604e319d37c065120487a7ce2ff9
Anthony Penna <penna1296@gmail.com>
2022-11-19 15:56:56 +0100

    feat(app): render markets on map, and add market filter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,55 @@
 <script setup lang="ts">
-import { MapboxMap } from "vue-mapbox-ts";
+import { MapboxMap, MapboxMarker, MapboxPopup } from "vue-mapbox-ts";
+import { useMarkets } from "@/composables/useMarkets";
 
 const accessToken = import.meta.env.VITE_APP_MAPBOX_ACCESS_TOKEN;
+const { getMarkets, markets, query } = useMarkets();
 </script>
 
 <template>
   <main class="h-screen">
-    <MapboxMap :accessToken="accessToken" />
+    <section
+      class="fixed bottom-8 left-2/4 bg-white rounded-4xl shadow-xl z-10 px-8 py-6 w-11/12 max-w-[800px]"
+      style="transform: translateX(-50%)"
+    >
+      <form class="w-full space-y-6">
+        <label for="query" class="font-medium text-xl text-center block">
+          Find weekly markets and flea markets in Berlin
+        </label>
+        <input
+          type="text"
+          id="query"
+          v-model="query"
+          placeholder="Search by name, district, address etc..."
+          class="bg-gray-100 px-5 py-3 w-full outline-[#4668F2] transition rounded-full"
+        />
+      </form>
+    </section>
+    <MapboxMap
+      :accessToken="accessToken"
+      :center="{ lat: 52.5, lon: 13.5 }"
+      :zoom="10.5"
+      mapStyle="light-v10"
+      @loaded="getMarkets()"
+    >
+      <MapboxMarker
+        v-for="market of markets"
+        :key="market.id"
+        :lngLat="{
+          lat: market.coordinates.latitude,
+          lon: market.coordinates.longitude,
+        }"
+      >
+        <MapboxPopup>
+          <ul>
+            <li>{{ market.name }}</li>
+            <li>
+              {{ market.address.street }}, {{ market.address.postalCode }},
+              {{ market.address.district }}
+            </li>
+          </ul>
+        </MapboxPopup>
+      </MapboxMarker>
+    </MapboxMap>
   </main>
 </template>
